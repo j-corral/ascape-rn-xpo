655168480d341630930a6c912a182a6477a41c5b
Jonathan Corral <johnlejardinnier@gmail.com>
2020-01-19 17:35:11 +0100

    feat(templates): add gmap on room-map template

--- components/templates/rooms/room-list/index.js
+++ components/templates/rooms/room-list/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
-import {Block, Text, NavBar, theme, Switch, Icon} from 'galio-framework';
+import {Block, Text} from 'galio-framework';
 
 // Molecules import
-import RoomToolbar from "../../../organisms/rooms/room-toolbar";
+
 
 export default class RoomListTemplate extends React.Component{
     render() {
--- components/templates/rooms/room-map/index.js
+++ components/templates/rooms/room-map/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
-import {Block, Text, NavBar, theme, Switch, Icon} from 'galio-framework';
+import {StyleSheet, Dimensions} from 'react-native';
+import {Block} from 'galio-framework';
+import MapView from 'react-native-maps';
 
 // Organisms import
 import RoomToolbar from "../../../organisms/rooms/room-toolbar";
@@ -9,9 +10,31 @@ export default class RoomMapTemplate extends React.Component{
 
     render() {
         return (
-            <Block>
-                <Text>Rooms Map</Text>
+            <Block flex={1} middle>
+                <MapView style={styles.mapStyle} region={Props.region} />
             </Block>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    mapStyle: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height - 200,
+    },
+});
+
+const Props = {
+    region: {
+        latitude: 43.6008029,
+        longitude: 1.3628014,
+        latitudeDelta: 0,
+        longitudeDelta: 1,
+    },
+};
