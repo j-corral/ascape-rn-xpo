a4c266283c02765664869d0a7372e1b76315236b
Jonathan Corral <johnlejardinnier@gmail.com>
2020-01-19 10:29:09 +0100

    chore(navigation, screens): remove old tab navigation and screens

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,20 +2,16 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 // navigators import
-import MainTabNavigator from './MainTabNavigator';
-import MainNavigator from './main-navigation/MainNavigator';
 import AuthNavigator from './AuthNavigator';
+import MainNavigator from './main-navigation/MainNavigator';
 
 import AuthLoadingScreen from "../screens/auth/AuthLoadingScreen";
 
 export default createAppContainer(
   createSwitchNavigator(
       {
-        // You could add another route here for authentication.
-        // Read more at https://reactnavigation.org/docs/en/auth-flow.html
         AuthLoading: AuthLoadingScreen,
         Auth: AuthNavigator,
-        // Main: MainTabNavigator,
         Main: MainNavigator,
     },
     {
--- navigation/AuthNavigator.js
+++ navigation/AuthNavigator.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Platform, StyleSheet } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 
 import SignInScreen from '../screens/auth/SignInScreen';
 import SignUpScreen from '../screens/auth/SignUpScreen';
 import ResetPasswordScreen from '../screens/auth/ResetPasswordScreen';
-import VerticalGradient from "../components/atoms/gradients/vertical-gradient";
 
 const AuthNavigator = createStackNavigator(
   {
