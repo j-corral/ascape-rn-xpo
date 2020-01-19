bb2637ab57e612f534d19c960b832140e8b3d37c
Jonathan Corral <johnlejardinnier@gmail.com>
2020-01-19 15:41:18 +0100

    feat(screens): create Rooms screen

---
+++ screens/rooms/RoomsScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import RoomListTemplate from "../../components/templates/rooms/room-list";
+import RoomsTemplate from "../../components/templates/rooms";
+
+export default class RoomsScreen extends React.Component {
+    render() {
+        return (
+            <RoomsTemplate />
+        )
+    }
+}
