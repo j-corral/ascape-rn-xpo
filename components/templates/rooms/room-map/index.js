9e961ff5612c043aa6ce6ee4a1d01a0affc13d4d
Jonathan Corral <johnlejardinnier@gmail.com>
2020-01-19 20:27:17 +0100

    fix(dependencies): save react-native-maps

--- components/templates/rooms/room-map/index.js
+++ components/templates/rooms/room-map/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import {StyleSheet, Dimensions} from 'react-native';
-import {Block, Text} from 'galio-framework';
-// import MapView from 'react-native-maps';
+import {Block} from 'galio-framework';
+import MapView from 'react-native-maps';
 
 // Organisms import
-import RoomToolbar from "../../../organisms/rooms/room-toolbar";
 
 export default class RoomMapTemplate extends React.Component{
 
     render() {
         return (
             <Block flex={1} middle>
-                <Text>room map</Text>
-                {/*<MapView style={styles.mapStyle} region={Props.region} />*/}
+                <MapView style={styles.mapStyle} region={Props.region} />
             </Block>
         )
     }
