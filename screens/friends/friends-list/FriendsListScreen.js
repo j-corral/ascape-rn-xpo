3fe265c9e78dad4068ec7eed55eaf14754e5fcf4
Jonathan Corral <johnlejardinnier@gmail.com>
2020-01-22 22:39:00 +0100

    feat(templates): create empty friends-list template

--- screens/friends/friends-list/FriendsListScreen.js
+++ screens/friends/friends-list/FriendsListScreen.js
@@ -1,12 +1,11 @@
 import React from "react";
 import {Block, Text} from 'galio-framework';
+import FriendsListTemplate from "../../../components/templates/friends/friends-list";
 
 export default class FriendsListScreen extends React.Component {
     render() {
         return (
-            <Block safe flex={1}>
-                <Text>My friends</Text>
-            </Block>
+            <FriendsListTemplate/>
         )
     }
 }
